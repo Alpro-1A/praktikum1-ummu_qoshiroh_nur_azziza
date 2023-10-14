36fcb673daaa72b9da1e36583caec7a70254ed26
ummuqoshiroh <2310631170055@student.unsika.ac.id>
2023-10-14 19:41:42 +0700

    menambahkan file main.cpp

---
+++ main.cpp/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Nama: Ummu Qoshrioh Nur Azziza" << endl << endl;
+    cout << "NPM: 2310631170055" << endl << endl;
+    cout << "Kelas: 1A-Informatika" << endl << endl;
+    cout << "Cita cita: web developer" << endl << endl;
+    cout << "motivasi masuk informatika: ingin berkecimbung di dunia teknologi karena sekarang sudah era digitalisasi" << endl;
+    return 0;
+}
