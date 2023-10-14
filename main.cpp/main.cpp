b49f79cc69453ba8489a8a3769488446f49b60ef
ummu <147624334+ummuqoshiroh@users.noreply.github.com>
2023-10-14 20:04:36 +0700

    Update main.cpp

--- main.cpp/main.cpp
+++ main.cpp/main.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 
 int main(){
+    cout << "-----------------------------------------------------------------" << endl;
     cout << "Nama: Ummu Qoshrioh Nur Azziza" << endl << endl;
     cout << "NPM: 2310631170055" << endl << endl;
     cout << "Kelas: 1A-Informatika" << endl << endl;
     cout << "Cita cita: web developer" << endl << endl;
-    cout << "motivasi masuk informatika: ingin berkecimbung di dunia teknologi karena sekarang sudah era digitalisasi" << endl;
+    cout << "motivasi masuk informatika: ingin berkecimbung di dunia teknologi" << endl;
+    cout << "-----------------------------------------------------------------"
     return 0;
 }
